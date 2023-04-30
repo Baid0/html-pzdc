2851ea09a91877012c214c5485d47b85e27b42ba
LBaido <baido@pop-os.localdomain>
2023-04-30 15:16:14 +0400

    pzdc komit

---
+++ Lecture #5/script.js
@@ -0,0 +1,13 @@
+var a = NaN; //not a number
+var b = new Date();
+var c = null;
+
+
+var person = {
+    name: "Giorgi",
+    age: 24,
+    date: new Date()
+}
+
+person.age = 20;
+console.log(person.name)
